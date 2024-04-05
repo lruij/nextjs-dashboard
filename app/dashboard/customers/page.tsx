1f9757f9314653de68e23c03bd1eda438b2a70f7
lruij <1012232443@qq.com>
2024-04-05 14:45:36 +0800

    fix: customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,35 @@
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import Table from '@/app/ui/customers/table';
+// import Search from '@/app/ui/search';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div> */}
+      <Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+    </div>
+  );
 }
